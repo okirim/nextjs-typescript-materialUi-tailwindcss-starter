e5f3da20eae5e88ace3ad096e94d8037a23865af
okirim <okirimkadiro@gmail.com>
2021-03-21 20:59:31 +0100

    starter

---
+++ pages/index.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Container from "@material-ui/core/Container";
+import Typography from "@material-ui/core/Typography";
+import Box from "@material-ui/core/Box";
+
+import Link from "../src/Link";
+
+
+export default function Index() {
+  return (
+    <Container maxWidth="sm">
+      <Box my={4}>
+        <Typography variant="h4" component="h1" gutterBottom>
+          Next.js with TypeScript example
+        </Typography>
+        <Link href="/about" color="secondary">
+          Go to the about page
+        </Link>
+      </Box>
+    </Container>
+  );
+}
